7d3ff76046ddae6519ae15bb66d342c9d1a5f1e6
Juan <juan_sparisci@hotmail.com>
2018-02-01 22:38:27 -0300

    Clase 44. Atributos personalizados

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+import { FormsModule } from '@angular/forms';
+
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
 
@@ -22,7 +24,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 export class PagesModule {}
--- adminpro/src/app/pages/progress/progress.component.ts
+++ adminpro/src/app/pages/progress/progress.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProgressComponent implements OnInit {
-
+  progreso: number = 50;
   constructor() { }
 
   ngOnInit() {
+  }
+  cambiarValor(val: number) {
+      this.progreso = parseFloat(this.progreso.toString());
+      const aux: number = this.progreso + val;
+      if (aux > 100) {
+        this.progreso = 100;
+        return;
+      }
+      if (aux < 0) {
+        this.progreso = 0;
+        return;
+      }
+      this.progreso = this.progreso + val;
   }
 
 }
